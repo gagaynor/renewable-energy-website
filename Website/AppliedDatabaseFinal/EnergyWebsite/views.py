527032e0e086d4d4a85e10b5b213193ae3d46cff
Gabe Gaynor <ggaynor@bremc.com>
2023-04-18 12:19:13 -0400

    website framework, about page, home page url, nav bar setup.

--- Website/AppliedDatabaseFinal/EnergyWebsite/views.py
+++ Website/AppliedDatabaseFinal/EnergyWebsite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """
+    View for the home page of the Django application.
+    """
+    return render(request, 'home.html')
+
+def about(request):
+    """
+    View for the about page of the Django application.
+    """
+    return render(request, 'about.html')
