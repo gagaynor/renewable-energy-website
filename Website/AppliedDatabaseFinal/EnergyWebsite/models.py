a3d0a15734894e44559022baebcb8050e4cfc4c3
Gabe Gaynor <ggaynor@bremc.com>
2023-04-22 14:15:36 -0400

    removed extra comments

--- Website/AppliedDatabaseFinal/EnergyWebsite/models.py
+++ Website/AppliedDatabaseFinal/EnergyWebsite/models.py
@@ -1,75 +1,68 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
-# Create your models here.
-
-class Energy(models.Model):
-    time = models.CharField(max_length=50, db_column='time',verbose_name='Date and Time')
-    biomass = models.IntegerField(db_column='generation biomass',verbose_name='Biomass')
-    brown_coal = models.IntegerField(db_column='generation fossil brown coal/lignite',verbose_name='Brown Coal')
-    coal_derived_gas = models.IntegerField(db_column='generation fossil coal-derived gas',verbose_name='Coal-Derived Gas')
-    gas = models.IntegerField(db_column='generation fossil gas',verbose_name='Gas')
-    hard_coal = models.IntegerField(db_column='generation fossil hard coal',verbose_name='Hard Coal')
-    oil = models.IntegerField(db_column='generation fossil oil',verbose_name='Oil')
-    oil_shale = models.IntegerField(db_column='generation fossil oil shale',verbose_name='Oil Shale')
-    peat = models.IntegerField(db_column='generation fossil prat',verbose_name='Peat')
-    geothermal = models.IntegerField(db_column='generation geothermal',verbose_name='Geothermal')
-    hydro_pumped_storage = models.IntegerField(db_column='generation hydro pumped storage consumption',verbose_name='Pumped Storage Consumption')
-    hydro_ror = models.IntegerField(db_column='generation hydro run-of-river and poundage',verbose_name='Run-of-River')
-    hydro_water_res = models.IntegerField(db_column='generation hydro water reservoir',verbose_name='Water Reservoir')
-    marine = models.IntegerField(db_column='generation marine',verbose_name='Marine')
-    nuclear = models.IntegerField(db_column='generation nuclear',verbose_name='Nuclear')
-    other_renewable = models.IntegerField(db_column='generation other renewable',verbose_name='Other Renewable')
-    solar = models.IntegerField(db_column='generation solar',verbose_name='Solar')
-    waste = models.IntegerField(db_column='generation waste',verbose_name='Waste')
-    offshore = models.IntegerField(db_column='generation wind offshore',verbose_name='Offshore Wind')
-    onshore = models.IntegerField(db_column='generation wind onshore',verbose_name='Onshore Wind')
-    forecast_day_ahead_solar = models.IntegerField(db_column='forecast solar day ahead',verbose_name='Solar Forecast')
-    forecast_day_ahead_offshore = models.IntegerField(db_column='forecast wind offshore eday ahead',verbose_name='Offshore Wind Forecast')
-    forecast_day_ahead_onshore = models.IntegerField(db_column='forecast wind onshore day ahead',verbose_name='Onshore Wind Forecast')
-    forecast_total_load = models.IntegerField(db_column='total_load_forecast',verbose_name='Total Load Forecast')
-    total_load_actual = models.FloatField(db_column='total_load_actual',verbose_name='Total Load Actual')
-    price_day_ahead = models.FloatField(db_column='price day ahead',verbose_name='Day-Ahead Price')
-    price_actual = models.FloatField(db_column='price actual',verbose_name='Price Actual')
+class EnergyDataset(models.Model):
+    time = models.TextField(blank=True, null=True)
+    generation_biomass = models.FloatField(db_column='generation biomass', blank=True, null=True)  
+    generation_fossil_brown_coal_lignite = models.FloatField(db_column='generation fossil brown coal/lignite', blank=True, null=True)  
+    generation_fossil_coal_derived_gas = models.FloatField(db_column='generation fossil coal-derived gas', blank=True, null=True)  
+    generation_fossil_gas = models.FloatField(db_column='generation fossil gas', blank=True, null=True)  
+    generation_fossil_hard_coal = models.FloatField(db_column='generation fossil hard coal', blank=True, null=True)  
+    generation_fossil_oil = models.FloatField(db_column='generation fossil oil', blank=True, null=True)  
+    generation_fossil_oil_shale = models.FloatField(db_column='generation fossil oil shale', blank=True, null=True)  
+    generation_fossil_peat = models.FloatField(db_column='generation fossil peat', blank=True, null=True)  
+    generation_geothermal = models.FloatField(db_column='generation geothermal', blank=True, null=True)  
+    generation_hydro_pumped_storage_aggregated = models.FloatField(db_column='generation hydro pumped storage aggregated', blank=True, null=True)  
+    generation_hydro_pumped_storage_consumption = models.FloatField(db_column='generation hydro pumped storage consumption', blank=True, null=True)  
+    generation_hydro_run_of_river_and_poundage = models.FloatField(db_column='generation hydro run-of-river and poundage', blank=True, null=True)  
+    generation_hydro_water_reservoir = models.FloatField(db_column='generation hydro water reservoir', blank=True, null=True)  
+    generation_marine = models.FloatField(db_column='generation marine', blank=True, null=True)  
+    generation_nuclear = models.FloatField(db_column='generation nuclear', blank=True, null=True)  
+    generation_other = models.FloatField(db_column='generation other', blank=True, null=True)  
+    generation_other_renewable = models.FloatField(db_column='generation other renewable', blank=True, null=True)  
+    generation_solar = models.FloatField(db_column='generation solar', blank=True, null=True)  
+    generation_waste = models.FloatField(db_column='generation waste', blank=True, null=True)  
+    generation_wind_offshore = models.FloatField(db_column='generation wind offshore', blank=True, null=True)  
+    generation_wind_onshore = models.FloatField(db_column='generation wind onshore', blank=True, null=True)  
+    forecast_solar_day_ahead = models.IntegerField(db_column='forecast solar day ahead', blank=True, null=True)  
+    forecast_wind_offshore_eday_ahead = models.FloatField(db_column='forecast wind offshore eday ahead', blank=True, null=True)  
+    forecast_wind_onshore_day_ahead = models.IntegerField(db_column='forecast wind onshore day ahead', blank=True, null=True)  
+    total_load_forecast = models.IntegerField(blank=True, null=True)
+    total_load_actual = models.FloatField(blank=True, null=True)
+    price_day_ahead = models.FloatField(db_column='price day ahead', blank=True, null=True)  
+    price_actual = models.FloatField(db_column='price actual', blank=True, null=True)  
 
     class Meta:
+        managed = False
         db_table = 'energy_dataset'
 
-    
 
-
-class Date(models.Model):
-    time = models.CharField(max_length=50, db_column='time',verbose_name='Date and Time')
-    
-    class Meta:
-        db_table = 'time_stamps'
-
-
-class Fossil(models.Model):
-    time = models.CharField(max_length=50, db_column='time',verbose_name='Date and Time')
-    brown_coal = models.IntegerField(db_column='generation fossil brown coal/lignite',verbose_name='Brown Coal')
-    gas = models.IntegerField(db_column='generation fossil gas',verbose_name='Gas')
-    hard_coal = models.IntegerField(db_column='generation fossil hard coal',verbose_name='Hard Coal')
-    oil = models.IntegerField(db_column='generation fossil oil',verbose_name='Oil')
+class FossilFuelGeneration(models.Model):
+    time = models.TextField(db_column='TIME', blank=True, null=True)
+    gen_brown_coal_lignite = models.FloatField(db_column='GEN_BROWN_COAL_LIGNITE', blank=True, null=True)  
+    gen_gas = models.FloatField(db_column='GEN_GAS', blank=True, null=True)  
+    gen_hard_coal = models.FloatField(db_column='GEN_HARD_COAL', blank=True, null=True)
+    gen_oil = models.FloatField(db_column='GEN_OIL', blank=True, null=True)  
 
     class Meta:
+        managed = False
         db_table = 'fossil_fuel_generation'
 
 
-class Renewable(models.Model):
-    time = models.CharField(max_length=50, db_column='time',verbose_name='Date and Time')
-    geothermal = models.IntegerField(db_column='generation geothermal',verbose_name='Geothermal')
-    hydro_pumped_storage = models.IntegerField(db_column='generation hydro pumped storage consumption',verbose_name='Pumped Storage Consumption')
-    hydro_ror = models.IntegerField(db_column='generation hydro run-of-river and poundage',verbose_name='Run-of-River')
-    hydro_water_res = models.IntegerField(db_column='generation hydro water reservoir',verbose_name='Water Reservoir')
-    nuclear = models.IntegerField(db_column='generation nuclear',verbose_name='Nuclear')
-    other_renewable = models.IntegerField(db_column='generation other renewable',verbose_name='Other Renewable')
-    solar = models.IntegerField(db_column='generation solar',verbose_name='Solar')
-    waste = models.IntegerField(db_column='generation waste',verbose_name='Waste')
-    onshore = models.IntegerField(db_column='generation wind onshore',verbose_name='Onshore Wind')
-    
+class Load(models.Model):
+    row_key = models.IntegerField(blank=True, null=True)
+    time = models.TextField(primary_key=True, blank=True, null=True)
+    forcasted_load = models.IntegerField(blank=True, null=True)
+    actual_load = models.IntegerField(blank=True, null=True)
+
     class Meta:
-        db_table = 'renewable_fuel_generation'
+        managed = False
+        db_table = 'load'
+
+
+class TimeStamps(models.Model):
+    row = models.AutoField(primary_key=True, blank=True, null=True)
+    time = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'time_stamps'
