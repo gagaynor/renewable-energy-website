4eb4f886435c67987f71c0e13ce73bfbe815fd8c
Gabe Gaynor <ggaynor@bremc.com>
2023-04-22 14:08:35 -0400

    added database

--- Website/AppliedDatabaseFinal/AppliedDatabaseFinal/settings.py
+++ Website/AppliedDatabaseFinal/AppliedDatabaseFinal/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -78,6 +77,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'renewables': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'renewable_spain.db',
+        # 'USER': 'mysql_user',
+        # 'PASSWORD': 'priv4te'
     }
 }
 
@@ -117,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
